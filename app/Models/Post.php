f87c7fae80ca3ccabb005ef8047fa46b8c4d8f75
masterF92 <cyber_master@hotmail.fr>
2025-10-31 00:46:31 -0400

    mise en place des tags ok

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,7 +11,7 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $with = ['category'];
+    protected $with = ['category','tags'];
 
     public function getRouteKeyName(): string
     {
